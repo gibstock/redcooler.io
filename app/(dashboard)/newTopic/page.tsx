cffeac4bba3199327ba0bc57da7e50b3c7a91929
Andre Gonzales <gibstock@gmail.com>
2023-06-01 17:54:03 -0700

    Adding NewTopic imports

--- app/(dashboard)/newTopic/page.tsx
+++ app/(dashboard)/newTopic/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { useUserStore } from '@/hooks/store';
+import api from '@/api/api';
+import { Server } from '@/utils/appwriteConfig';
+import { useRouter } from 'next/navigation';
+
+// NEED
+/*
+* Topics database
+* User from zustand
+* API
+* Server
+* useRouter
+*/
 
 const NewTopic = () => {
+  
+  const user = useUserStore(state => state.user);
+
   return (
     <div>
       <h1>New Topic</h1>
+      <p>User: {user?.name}</p>
     </div>
   )
 }
