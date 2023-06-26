36ba858e04a3c722680eaa057fb06031d29522b3
Andre Gonzales <gibstock@gmail.com>
2023-06-25 19:24:14 -0700

    Added content guidelines route and content

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="footer-content flex flex-row justify-center items-center gap-4 bg-transparent py-4">
+    <footer className='py-4'>
+      <div className="links text-slate-400 text-center">
+        <Link href={'/content-guideline'}>
+          <span>Content-Guideline</span>
+        </Link>
+      </div>
+      <div className="footer-content flex flex-row justify-center items-center gap-4 bg-transparent">
         <span className='text-slate-400'>Copyright &copy; 2023 </span>
         <span className='font-bold text-red-700'>Redcooler.io</span>
       </div>
