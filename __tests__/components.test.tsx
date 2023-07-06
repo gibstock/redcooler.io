8b235e9fe2ffd02597f091d99a6b53b464c88c46
Andre Gonzales <gibstock@gmail.com>
2023-07-05 19:40:48 -0700

    Added jest and started component testing

---
+++ __tests__/components.test.tsx
@@ -0,0 +1,49 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import Footer from '@/components/Footer';
+import Button from '@/components/Button';
+
+const defaultButtonProps = {
+  label: 'Click Me',
+  bgColor: 'bg-black',
+  fontColor: 'text-red-500',
+  padding: 'p-4',
+  hover: 'hover:bg-white',
+  disabled: false,
+  disabledConditions: '',
+  outline: '',
+  type: undefined,
+  fullWidth: false,
+}
+
+describe("footer component", () => {
+  test("renders Redcooler.io in footer", () => {
+    render(<Footer />);
+
+    const company = screen.getByText('Redcooler.io');
+
+    expect(company).toBeInTheDocument();
+
+  })
+})
+
+describe("button component", () => {
+  it("renders a buton", () => {
+    render(<Button {...defaultButtonProps} />);
+
+    const button = screen.getByRole("button");
+
+    expect(button).toBeInTheDocument();
+  })
+
+  it("calls the onClick function when the button is clicked", () => {
+    const handleClick = jest.fn();
+    render(<Button onClick={handleClick} {...defaultButtonProps} />);
+    
+    const button = screen.getByRole("button");
+
+    fireEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  })
+})
+
